be3778b1f3386f5332ad501c041f1ea7528a4902
ahmad081177 <ahmad081177@gmail.com>
2024-03-01 09:59:43 +0200

    Send Email & Service injection

---
+++ 07- Send Email and Services/HousingWebApp/Models/AppRegister.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HousingWebApp.Models
+{
+    public class AppRegister
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        [Compare("Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ 07- Send Email and Services/HousingWebApp/Services/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace HousingWebApp.Services
+{
+    public class EmailService
+    {
+        private IConfiguration Config;
+        public EmailService(IConfiguration config)
+        {
+            Config = config;
+        }
+
+        public async Task SendEmailAsync(string toemail, string subject, 
+            string message, bool isHtml=true)
+        {
+            var section = Config.GetSection("Email");
+            //TBD - check if section is null...
+            if(section != null)
+            {
+                string from_email = section["from:mail"];
+                string from_displayName = section["from:display"];
+                string password = section["password"];
+
+                var fromEmail = new MailAddress(from_email, from_displayName);
+                var toEmail = new MailAddress(toemail);
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Timeout = 5000,
+                    Credentials = new NetworkCredential(fromEmail.Address, password)
+                };
+                using var messageObj = new MailMessage(fromEmail, toEmail)
+                {
+                    Subject = subject,
+                    Body = message,
+                    IsBodyHtml = isHtml
+                };
+                try
+                {
+                    await smtp.SendMailAsync(messageObj);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
